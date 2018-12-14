aca5e63661d93011d904c28ef781baeb4c14d9ae
Roman Demyanchuk <44296332+hvllo@users.noreply.github.com>
2018-12-14 15:44:17 +0200

    Force commit

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from account.models import User
+from post.models import Post, Like
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
@@ -11,3 +12,12 @@ class UserProfileSerializer(serializers.ModelSerializer):
         fields = ('id', 'email', 'date_joined',
                   'first_name', 'last_name', 'password')
         extra_kwargs = {'password': {'write_only': True}}
+
+
+class PostSerializer(serializers.ModelSerializer):
+    date_joined = serializers.ReadOnlyField
+
+    class Meta:
+        model = Post
+        fields = ('id', 'user', 'title',
+                  'body')
